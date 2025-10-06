6f19f2decb4ce350d20ea3e0cb7d0cca507b6f43
asraf705 <asrafuzzaman705@gmail.com>
2025-10-06 20:31:27 +0600

    modified ProductController.php in Backend

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -7,5 +7,17 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function productView(){
+        return view('admin-panel.pages.product.index');
+    }
+
+    public function productAdd(){
+        return view('admin-panel.pages.product.add');
+    }
+
+    public function productStore(Request $request){
+        return view('admin-panel.pages.product.add');
+    }
+
+
 }
