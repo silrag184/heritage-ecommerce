e8763620141bac0da7cb0e46a381fc1ea99bcba9
silrag184 <msilrag@gmail.com>
2025-10-06 15:46:27 +0600

    Add views for adding and editing products in the admin panel

---
+++ resources/views/admin-panel/pages/product/add.blade.php
@@ -0,0 +1,9 @@
+@extends('admin-panel.layout.app')
+
+@section('title')
+Add Product
+@endsection
+
+@section('admin-content')
+
+@endsection
---
+++ resources/views/admin-panel/pages/product/edit.blade.php
@@ -0,0 +1,9 @@
+@extends('admin-panel.layout.app')
+
+@section('title')
+Edit Product
+@endsection
+
+@section('admin-content')
+
+@endsection
